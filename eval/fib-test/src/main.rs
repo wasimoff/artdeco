4d80fd2e08697636109cbd76ec701bf73eccc9d4
Julius Lehmann <lehmanju@devpi.de>
2025-09-10 11:22:43 +0200

    fix: fixup eval/fib-test for offloading demo

---
+++ eval/fib-test/src/main.rs
@@ -0,0 +1,41 @@
+use artdeco::{
+    nats::daemon_nats,
+    scheduler::fixed::Fixed,
+    task::{TaskExecutable, Workload},
+};
+use futures::sink::drain;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
+use tracing::Level;
+
+const FIBBONACCI_WASM: &[u8; 59778] = include_bytes!("../fibbonacci.wasm");
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .map_err(|_err| eprintln!("Unable to set global default subscriber"))
+        .unwrap();
+
+    // Create a task queue
+    let (sender, receiver) = mpsc::channel(1);
+
+    // Create a workload
+    let exec = TaskExecutable::new(FIBBONACCI_WASM as &[u8]);
+    let workload = Workload {
+        executable: exec,
+        args: vec!["fibonacci.wasm".to_owned(), "10".to_owned()],
+        response_channel: drain(),
+    };
+
+    // Send the workload to the queue
+    sender.send(workload).await?;
+    //drop(sender); // drop sender so the daemon stops after offloading all tasks
+
+    // Create a scheduler
+    let scheduler = Fixed::new();
+    let receiver_stream = ReceiverStream::new(receiver);
+    daemon_nats(receiver_stream, scheduler, "nats").await
+}
--- wasi-apps/fibonacci/README.txt
+++
@@ -1,2 +0,0 @@
-This is a copy of ../helloworld/rust with the sole purpose
-of providing a simple fibonacci workload binary for the webprovider.
